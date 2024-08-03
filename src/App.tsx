674b1ba909a4f53590bd1c819df5800a5b61924b
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2024-08-03 17:09:25 -0300

    feat: Implement routes to /patient route

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
 import './App.css'
+import { Switch, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import PatientForm from './pages/PatientForm'
 
 function App() {
 
   return (
-    <>
+    <div>
       <h1>Vet form</h1>
-    </>
+      <Switch>
+        <Route
+          component={Home}
+          exact
+          path="/"
+        />
+        <Route
+          component={PatientForm}
+          path="/patient"
+        />
+      </Switch>
+    </div>
   )
 }
 
